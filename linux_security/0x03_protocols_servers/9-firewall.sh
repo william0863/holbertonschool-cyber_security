e0150412e027c637b4734261d4d823cdc59bec1a
william0863 <granger.w35@gmail.com>
2024-10-20 23:54:59 +0200

    task9

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport ssh 22 -j ACCEPT && iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport ssh 22 -j ACCEPT && iptables -A INPUT -m state --state RELATED,ESTABLISHED -j DROP ACCEPT
