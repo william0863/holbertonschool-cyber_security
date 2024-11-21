a1cbc6b6397ac54c814967323c8d37a1910759c6
william0863 <granger.w35@gmail.com>
2024-11-21 04:01:53 +0100

    task0

---
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <log_file>"
+    exit 1
+fi
+
+LOG_FILE="$1"
+
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Error: File '$LOG_FILE' not found."
+    exit 1
+fi
+
+MOST_FREQUENT_IP=$(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1)
+
+echo "The IP responsible for the most requests is:"
+echo "$MOST_FREQUENT_IP"
+
