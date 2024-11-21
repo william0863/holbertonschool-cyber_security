dc4d75198abf460b21dc66a82c673d7c4986fd28
william0863 <granger.w35@gmail.com>
2024-11-21 05:44:41 +0100

    task0 update

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -12,7 +12,7 @@ if [ ! -f "$LOG_FILE" ]; then
     exit 1
 fi
 
-MOST_FREQUENT_IP=$(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1)
+MOST_FREQUENT_IP=$(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
 
 echo "$MOST_FREQUENT_IP"
 
