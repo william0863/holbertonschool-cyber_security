4b28208f9f3f9e56f49a927bd57f7708155f27d0
william0863 <granger.w35@gmail.com>
2024-11-27 02:38:50 +0100

    task3

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon -oN comprehensive_scan_results.txt $1
+nmap --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon $1 -oN comprehensive_scan_results.txt
