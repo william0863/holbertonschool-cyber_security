e4430cbb43ad3727be9a9ccf24d3a1f4000b4cb3
W <granger.w35@gmail.com>
2024-11-27 02:48:19 +0100

    Update 5-service_enumeration.sh

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sV -O --script "banner,ssl-enum-ciphers,default,smb-enum-domains" --traceroute $1 -oN service_enumeration_results.txt
+nmap -sV -A --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
