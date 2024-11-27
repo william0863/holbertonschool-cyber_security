baa21b7f0861515c7acf80a84ea0c80b7a85aa6a
W <granger.w35@gmail.com>
2024-11-27 02:41:23 +0100

    Update 3-comprehensive_scan.sh

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon -oN comprehensive_scan_results.txt $1
+nmap -oN comprehensive_scan_results.txt --script=http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon $1
