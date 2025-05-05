8e2af339ef87a1142d6f0b9e754bbc561b83a04a
william0863 <granger.w35@gmail.com>
2025-05-05 18:45:26 +0200

    creating files

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep -oP '(?<=user=)[^ ]+' auth.log | sort -u | paste -sd,
+grep "Accepted password" auth.log | awk '{print $9}' | grep -vE '^(root|invalid|app|backup|bin|daemon|dhcp|games|gnats|irc|klog|libuuid|list|lp|mail|man|news|nobody|ntp|proxy|sync|sys|syslog|uucp|www-data|single|sftp)$' | sort -u | paste -sd,
 
